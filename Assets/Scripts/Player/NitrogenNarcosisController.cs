eea59dd08ea37d69a6a65307ad4448e15bde1678
ZacAguas <zacaguas@gmail.com>
2023-09-12 23:50:50 +0100

    Started NitrogenNarcosisController script

--- Assets/Scripts/Player/NitrogenNarcosisController.cs
+++ Assets/Scripts/Player/NitrogenNarcosisController.cs
@@ -1,8 +1,35 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using IE.RichFX;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class NitrogenNarcosisController : MonoBehaviour
 {
+    [SerializeField] private Volume volume;
+    private VolumeProfile profile;
 
+    private int narcosisLevel;
+
+    // Level 1
+    private BloomStreak bloomStreak;
+    private DirectionalBlur directionalBlur;
+    private Sharpen sharpen;
+    // Level 2
+    private DisplaceView displaceView;
+    private Wobble wobble;
+
+
+    private void Start()
+    {
+        narcosisLevel = 0;
+        profile = volume.profile;
+
+        // bloomStreak = profile.TryGet<BloomStreak>(
+    }
+
+    private void Update()
+    {
+    }
 }
